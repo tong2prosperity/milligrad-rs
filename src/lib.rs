e36be456d09418f58943fa514f18d3be9de539cc
trevorlink <lvpidadiao@hotmail.com>
2024-03-26 00:02:48 +0800

    file creation

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+mod fundamental;
+mod nn;
+mod optim;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
